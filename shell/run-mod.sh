7fe0ff7aef32570bf657766e91c78f3a4b4c6271
RealDanTheMan <dantheman.py@gmail.com>
2025-01-28 20:03:19 +0000

    Ensred run script targets the mod and developer settings

--- shell/run-mod.sh
+++ shell/run-mod.sh
@@ -4,4 +4,4 @@ CWD=$(pwd)
 BUILD_PATH="$CWD/build/release-linux-x86_64"
 XFILE="$BUILD_PATH/ioquake3.x86_64"
 
-$XFILE
+"$XFILE" +set fs_basegame pure-qmod +fs_game pure-qmod +set developer 1 +set logfile 2 +set fs_debug 1
